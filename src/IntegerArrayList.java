c25d3fc0666d0b83d5cca3df343acf1e98ebf0a2
Sarah Guthals <sarah@guthals.com>
2021-08-16 09:29:55 -0700

    Initialize exercise-3-testing-get

---
+++ src/IntegerArrayList.java
@@ -0,0 +1,44 @@
+import org.apache.commons.lang3.NotImplementedException;
+
+/**
+ * An ordered collection (a.k.a. sequence) of integers that allows the user to append new elements and access elements by their index.
+ * Based on the Java Util List API: https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+ * Implemented using a standard Java array.
+ */
+public class IntegerArrayList {
+    
+    private int size;
+    private int[] data;
+
+    public IntegerArrayList() {
+        this.size = 0;
+        this.data = new int[10];
+    }
+
+    /**
+     * Returns the number of elements in this list. 
+     * @return the number of elements in this list
+     */
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     * @param index - index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException - if the index is out of range (index < 0 || index >= size())
+     */
+    public int get(int index) {
+        return this.data[index+1];
+    }
+
+    /**
+     * Appends the specified element to the end of this list.
+     * @param value - element to be appended to this list
+     */
+    public void add(int value) {
+        this.data[0] = value;
+        this.size += 1;
+    }
+}
---
+++ src/IntegerArrayListTest.java
@@ -0,0 +1,88 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IntegerArrayListTest {
+    private IntegerArrayList list;
+
+    @BeforeEach
+    public void setup() {
+        list = new IntegerArrayList();
+    }
+
+    @Test
+    public void testSizeEmptyList() {
+        // TODO
+    }
+
+    @Test
+    public void testSizeNonEmptyList() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingOneValueIncreasesSize() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingMultipleValuesIncreasesSize() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingNegativeValuesIncreasesSize() {
+        // TODO
+    }
+
+    @Test
+    public void testFirstValueIsStoredAtIndexZero() {
+        // TODO
+    }
+
+    @Test
+    public void testMultipleValuesAreStoredInOrderOfAdding() {
+        // TODO
+    }
+
+    @Test
+    public void testNegativeValuesAreStoredInOrderOfAdding() {
+        // TODO
+    }
+
+    @Test
+    public void testIndexEqualToSizeGivesIndexOutOfBoundsException() {
+        // TODO
+    }
+
+    @Test
+    public void testIndexLargerThanSizeGivesIndexOutOfBoundsException() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingElevenValuesIncreasesSizeToEleven() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingFifteenValuesIncreasesSizeToFifteen() {
+        // TODO
+    }
+
+    @Test
+    public void testCanAccessTheEleventhValueInAList() {
+        // TODO
+    }
+
+    @Test
+    public void testCanAccessTheFifteenthValueInAList() {
+        // TODO
+    }
+
+    @Test
+    public void testIndexLargerThanSizeGivesIndexOutOfBoundsExceptionForListOfSizeFifteen() {
+        // TODO
+    }
+}
