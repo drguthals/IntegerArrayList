69571e59ddd2dd7de95ba1fc5d5982ec40df540d
Sarah Guthals <sarah@guthals.com>
2021-08-16 09:29:55 -0700

    Initialize exercise-2-testing-add

---
+++ src/IntegerArrayListTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class IntegerArrayListTest {
+    private IntegerArrayList list;
+
+    @BeforeEach
+    public void setup() {
+        list = new IntegerArrayList();
+    }
+
+    @Test
+    public void testSizeEmptyList() {
+        // TODO
+    }
+
+    @Test
+    public void testSizeNonEmptyList() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingOneValueIncreasesSize() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingMultipleValuesIncreasesSize() {
+        // TODO
+    }
+
+    @Test
+    public void testAddingNegativeValuesIncreasesSize() {
+        // TODO
+    }
+}
